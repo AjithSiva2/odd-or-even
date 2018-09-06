77a9c92db112b48ef76110da51819912c78080cc
AjithSiva2 <ajithsiva12.ak@gmail.com>
2018-09-06 15:13:57 +0530

    Create odd or even.py

---
+++ odd or even.py
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	
+	int a;
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<"number is even";
+	}
+	else if(a%2==1)
+	{
+		cout<<"number is odd";
+	}
+	else
+	{
+	cout<<"number is invalid";
+	}
+	return 0;
+}
